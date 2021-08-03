e77fa2f6f2e5af349152a149663554f955409f7c
Gowri Jayaprakash <34152914+gowrijp@users.noreply.github.com>
2021-08-04 03:23:13 +0530

    finding number of islands/region of 1s

--- number-of-islands.cpp
+++ number-of-islands.cpp
@@ -1,5 +1,32 @@
 // https://leetcode.com/problems/number-of-islands/
 
+// SIMPLER RECURSIVE SOLUTION WITH NO EXTRA VISITED ARRAY USED ***
+class Solution {
+public:
+    //mark all the land as '2' of current island
+    void solve(vector<vector<char>>& grid, int i, int j, int m, int n){
+        if(i < 0 || j < 0 || i >= m || j >= n || grid[i][j] != '1') return;
+        grid[i][j] = '2';
+        
+        solve(grid, i-1, j, m, n);
+        solve(grid, i+1, j, m, n);
+        solve(grid, i, j-1, m, n);
+        solve(grid, i, j+1, m, n);
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size(), n = grid[0].size(), ans = 0;
+        if(m == 0) return 0;
+        
+        for(int i = 0; i < m; i++)
+            for(int j = 0; j < n; j++)
+                if(grid[i][j] == '1'){
+                    solve(grid, i, j, m, n);
+                    ans++;
+                }
+        return ans;
+    }
+};
+
 // My SOLUTION (BFS)
 
 class Solution {
@@ -49,31 +76,4 @@ public:
         return ans;
     }
     
-};
-
-// SIMPLER RECURSIVE SOLUTION
-class Solution {
-public:
-    //mark all the land as '2' of current island
-    void solve(vector<vector<char>>& grid, int i, int j, int m, int n){
-        if(i < 0 || j < 0 || i >= m || j >= n || grid[i][j] != '1') return;
-        grid[i][j] = '2';
-        
-        solve(grid, i-1, j, m, n);
-        solve(grid, i+1, j, m, n);
-        solve(grid, i, j-1, m, n);
-        solve(grid, i, j+1, m, n);
-    }
-    int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size(), n = grid[0].size(), ans = 0;
-        if(m == 0) return 0;
-        
-        for(int i = 0; i < m; i++)
-            for(int j = 0; j < n; j++)
-                if(grid[i][j] == '1'){
-                    solve(grid, i, j, m, n);
-                    ans++;
-                }
-        return ans;
-    }
 };
